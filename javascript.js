a8e583b3343760c990df724bca5da697b5bfb552
eudaanilo <dsa7@aluno.ifal.edu.br>
2023-03-16 20:55:16 -0300

    Adicionado a função nos outros botões que faltavam e adicionado as descrições

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-var bracos = document.getElementById('bracos');
+var bracos = document.getElementById("bracos");
+var blindagem = document.getElementById("blindagem");
+var nucleos = document.getElementById("nucleos");
+var pernas = document.getElementById("pernas");
 
-var botaoAumentaBracos = document.getElementById('aumenta-bracos');
+var botaoAumentaBracos = document.getElementById("aumenta-bracos");
 botaoAumentaBracos.addEventListener('click', function () {
     if (parseInt(bracos.value) < 10) {
         if (bracos.value == 9) {
@@ -11,14 +14,14 @@ botaoAumentaBracos.addEventListener('click', function () {
     }
 })
 
-var botaoDiminuiBracos = document.getElementById('diminui-bracos')
+var botaoDiminuiBracos = document.getElementById("diminui-bracos")
 botaoDiminuiBracos.addEventListener('click', function () {
     if (parseInt(bracos.value) > 0) {
         bracos.value = `0${--bracos.value}`
     }
 })
 
-var botaoAumentaBlindagem = document.getElementById('aumenta-blindagem');
+var botaoAumentaBlindagem = document.getElementById("aumenta-blindagem");
 botaoAumentaBlindagem.addEventListener('click', function () {
     if (parseInt(blindagem.value) < 10) {
         if (blindagem.value == 9) {
@@ -29,9 +32,43 @@ botaoAumentaBlindagem.addEventListener('click', function () {
     }
 })
 
-var botaoDiminuiBlindagem = document.getElementById('diminui-blindagem')
+var botaoDiminuiBlindagem = document.getElementById("diminui-blindagem")
 botaoDiminuiBlindagem.addEventListener('click', function () {
     if (parseInt(blindagem.value) > 0) {
         blindagem.value = `0${--blindagem.value}`
+    }
+})
+
+var botaoAumentaNucleos = document.getElementById("aumenta-nucleos");
+botaoAumentaNucleos.addEventListener('click', function () {
+    if (parseInt(blindagem.value) < 10) {
+        if (nucleos.value == 9) {
+            nucleos.value = ++nucleos.value
+        } else {
+            nucleos.value = `0${++nucleos.value}`
+        }
+    }
+})
+var botaoDiminuiNucleos = document.getElementById("diminui-nucleos")
+botaoDiminuiNucleos.addEventListener('click', function () {
+    if (parseInt(nucleos.value) > 0) {
+        nucleos.value = `0${--nucleos.value}`
+    }
+})
+
+var botaoAumentaPernas = document.getElementById("aumenta-pernas");
+botaoAumentaPernas.addEventListener('click', function () {
+    if (parseInt(pernas.value) < 10) {
+        if (pernas.value == 9) {
+            pernas.value = ++pernas.value
+        } else {
+            pernas.value = `0${++pernas.value}`
+        }
+    }
+})
+var botaoDiminuiPernas = document.getElementById("diminui-pernas")
+botaoDiminuiPernas.addEventListener('click', function () {
+    if (parseInt(pernas.value) > 0) {
+        pernas.value = `0${--pernas.value}`
     }
 })
